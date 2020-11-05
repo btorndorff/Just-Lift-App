183730f76662b1b7cb39cad4f9e64832fd1b9d94
Eric Armstrong <earmstrong_28@aol.com>
2020-11-05 18:47:29 -0500

    facebook login stuff

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -23,7 +23,7 @@ var firebaseConfig = {
   };
   // Initialize Firebase
 if (!firebaseApp.apps.length) {
-  firebaseApp.initializeApp(config);
+  firebaseApp.initializeApp(firebaseConfig);
 }
 
 export default class HomeScreen extends React.Component {
@@ -33,60 +33,95 @@ export default class HomeScreen extends React.Component {
     this.state = { loading: true, token: null };
   }
 
-  componentDidMount(){
-   setTimeout (()=>{this.checkForToken()},2000);
+  // componentDidMount(){
+  //  setTimeout (()=>{
+  //    this.checkForToken()
+  //   },2000);
+  //  this.checkForFirebaseCredential();
+  //   // Listen for authentication state to change.
+  //   //var user;
+  //   firebaseApp.auth().onAuthStateChanged(user => {
+  //     if (user != null) {
+  //       console.log('We are authenticated now!');
+  //       Alert.alert('We authneticated with Fireabse!', `Hi ${user}`);
+  //     }
+  //     else{
+  //       console.log('did not authenticate, user was null');
+  //     }
+  //   });
+  // }
 
-   this.checkForFirebaseCredential();
-    // Listen for authentication state to change.
+
+  // //Check Async Storage if token is available
+  // //If it is available set loading state to false 
+  // async checkForToken(){
+  //   let token = await SecureStore.getItemAsync('token')
+  //   this.setState({
+  //     token: token,
+  //     loading: false,
+  //   });
+  // }
+
+  // async checkForFirebaseCredential() {
+  //   let credential = await SecureStore.getItemAsync('firebaseCredential');
+  //   if (credential) {
+  //     firebaseApp
+  //       .auth()
+  //       .signInWithCredential(credential)
+  //       .catch(error => {
+  //         console.log('Auth failed and here the error' + JSON.stringify(error));
+  //       });
+  //   }
+  // }
+
+  // //Write token to secure storage. 
+  // async saveTokenToSecureStorage(token, credential){
+  //    SecureStore.setItemAsync('token', token);
+  //    //save firebase credential
+  //    SecureStore.setItemAsync('firebaseCredential', credential);
+  //    this.setState({
+  //      token: token
+  //    });
+  // }  
+
+  componentDidMount() {
     firebaseApp.auth().onAuthStateChanged(user => {
       if (user != null) {
-        console.log('We are authenticated now!');
-        Alert.alert('We authneticated with Fireabse!', `Hi ${user}`);
-      }
-      else{
-
-        console.log('did not authenticate, user was null');
+          console.log(user);
       }
     });
   }
 
+  async Facebooklogin() {
+    try{
+      await Facebook.initializeAsync({appId : '576677119727703', appName : 'JustLift'});
 
-  //Check Async Storage if token is available
-  //If it is available set loading state to false 
-  async checkForToken(){
-     let token = await SecureStore.getItemAsync('token')
-     //console.log(token)
-    this.setState({
-      token: token,
-      loading: false
-    })
-  }
-
-  async checkForFirebaseCredential() {
-    let credential = await SecureStore.getItemAsync('firebaseCredential');
-    if (credential) {
-      firebaseApp
-        .auth()
-        .signInWithCredential(credential)
-        .catch(error => {
-          console.log('Auth failed and here the error' + JSON.stringify(error));
+      const { type, token } = await
+        Facebook.logInWithReadPermissionsAsync(
+        {
+            permission: "public_profile"
         });
-    }
-  }
+      if (type === "success") {
 
-  //Write token to secure storage. 
-  async saveTokenToSecureStorage(token){
-     SecureStore.setItemAsync("token", token)
-     this.setState({
-       token: token
-     })
-  }  
+        const credential = firebaseApp.auth.FacebookAuthProvider.credential(token);
+
+        firebaseApp
+        .auth().signInWithCredential(credential).catch(error => {
+          console.log('Auth failed and here is the error(1) ' + JSON.stringify(error))
+        });
+      }
+    
+  }catch ({ message }) {
+         alert(`Facebook Login Error: ${message}`);
+  }
+}
+  
     
   render() {
-    if(this.state.loading === true){
-        return(<LoadingScreen/>)
-    }
-    else if(this.state.token === null){
+    // if(this.state.loading === true){
+    //     return(<LoadingScreen/>)
+    // }
+    //else if(this.state.token === null){
         return (
         <View style={styles.container}>
             <LinearGradient
@@ -102,66 +137,68 @@ export default class HomeScreen extends React.Component {
             }}>
             <Image source={require("../assets/dumbell.png")} style={styles.logo} /> 
             <Text style={styles.instructions}>
-            JustLift
+              JustLift
             </Text>
             <TouchableOpacity 
-            onPress={() => this.props.navigation.navigate('Login')} style={styles.button_login}>
-            <Text style={styles.buttonLText}>Login</Text>
+              onPress={() => this.props.navigation.navigate('Login')} style={styles.button_login}>
+              <Text style={styles.buttonLText}>Login</Text>
             </TouchableOpacity>
             <TouchableOpacity 
-            onPress={() => this.props.navigation.navigate('Register')} style={styles.button_register}>
-            <Text style={styles.buttonRText}>Register</Text>
+              onPress={() => this.props.navigation.navigate('Register')} style={styles.button_register}>
+              <Text style={styles.buttonRText}>Register</Text>
             </TouchableOpacity>
-            <Button title="Login With Facebook" onPress={()=>this.logIn()}/>
+            <TouchableOpacity 
+              onPress={()=>this.Facebooklogin()} style={styles.button_login}>
+              <Text style={styles.buttonLText}>Login With Facebook</Text>
+            </TouchableOpacity>
             </LinearGradient>
         </View>
         );
-    }
-    else{
-        return (<FeedScreen/>)
-    }
+    //}
+    // else{
+    //     return (<FeedScreen/>)
+    // }
   }
   
-  async logIn() {
-    try {
-      //Seed documentation on course site at mobileappdev.teachable.com
-      //For default user names and passwords.
-      await Facebook.initializeAsync('576677119727703');//576677119727703 //184462529575747
-      const {
-        type,
-        token,
-        expires,
-        permissions,
-        declinedPermissions,
-      } = await Facebook.logInWithReadPermissionsAsync({
-        permissions: ['public_profile'],
-      });
-      if (type === 'success') {
-        // Get the user's name using Facebook's Graph API
-        const response = await fetch(
-          `https://graph.facebook.com/me?access_token=${token}`
-        );
+  // async logIn() {
+  //   try {
+  //     //Seed documentation on course site at mobileappdev.teachable.com
+  //     //For default user names and passwords.
+  //     await Facebook.initializeAsync({appId : '576677119727703'});
+  //     const {
+  //       type,
+  //       token,
+  //       expires,
+  //       permissions,
+  //       declinedPermissions,
+  //     } = await Facebook.logInWithReadPermissionsAsync({
+  //       //permissions: ['public_profile'],
+  //     });
+  //     if (type === 'success') {
+  //       // Get the user's name using Facebook's Graph API
+  //       // const response = await fetch(
+  //       //   `https://graph.facebook.com/me?access_token=${token}`
+  //       // );
 
-        let credential = firebaseApp.auth.FacebookAuthProvider.credential(
-          token
-        );
-        firebaseApp
-          .auth()
-          .signInWithCredential(credential)
-          .catch(error => {
-            console.log(
-              'Auth failed and here is the error ' + JSON.stringify(error)
-            );
-          });
-
-        this.saveTokenToSecureStorage(token)
-        Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
-      } else {
-        // type === 'cancel'
-      }
-    } catch ({ message }) {
-      alert(`Facebook Login Error: ${message}`);
-    }
-  }
+  //       var credential = firebaseApp.auth.FacebookAuthProvider.credential(token);
+        
+  //       firebaseApp
+  //         .auth()
+  //         .signInWithCredential(credential)
+  //         .catch(error => {
+  //           console.log(
+  //             'Auth failed and here is the error(1) ' + JSON.stringify(error)
+  //           );
+  //         });
+        
+  //       this.saveTokenToSecureStorage(token, credential)
+  //       //Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+  //     } else {
+  //       // type === 'cancel'
+  //     }
+  //   } catch ({ message }) {
+  //     alert(`Facebook Login Error: ${message}`);
+  //   }
+  // }
 }
 
